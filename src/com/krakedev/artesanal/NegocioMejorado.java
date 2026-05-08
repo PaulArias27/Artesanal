d6d83312d56d05d41f2575009fdba42e91a4f30b
PaulArias27 <paularias2727@gmail.com>
2026-05-08 09:42:52 -0500

    PA: implementado metodo registrarCliente y test provocar error NullPointerException

--- src/com/krakedev/artesanal/NegocioMejorado.java
+++ src/com/krakedev/artesanal/NegocioMejorado.java
@@ -6,6 +6,10 @@ public class NegocioMejorado {
 	
 	private ArrayList<Maquina> maquinas = new ArrayList<>();
 	
+	private ArrayList<Cliente> clientes = new ArrayList<>();
+	
+	private int ultimoCodigo = 100;
+	
 	
 
 	public ArrayList<Maquina> getMaquinas() {
@@ -16,6 +20,7 @@ public class NegocioMejorado {
 		this.maquinas = maquinas;
 	}
 	
+	//parte 1
 	public String generarCodigo() {
 		int numero;
 		numero = (int)(Math.random()*100)+1;
@@ -25,11 +30,15 @@ public class NegocioMejorado {
 	public boolean agregarMaquina(String nombreCerveza, String descripcion,double precioPorMl) {
 		
 		String codigo = generarCodigo();
+		if(recuperarMaquina(codigo) != null) {
+			codigo = generarCodigo();
+		}
 		Maquina nueva = new Maquina(nombreCerveza,descripcion,precioPorMl,codigo);
 		
 		maquinas.add(nueva);
 		return true;
 	}
+	
 	public void cargarMaquinas() {
 		for(int i = 0; i < maquinas.size(); i ++) {
 			maquinas.get(i).llenarMaquina();
@@ -46,5 +55,28 @@ public class NegocioMejorado {
 		}
 		return null;
 	}
+	
+	//parte 2
+	
+	public void registrarCliente(String nombre, String cedula) {
+		
+		Cliente c = new Cliente(nombre,cedula);
+		c.setCodigo(ultimoCodigo);
+		ultimoCodigo ++;
+		clientes.add(c);
+	}
+	
+	public Cliente buscarClientePorCedula(String cedula) {
+		for(int i = 0; i < clientes.size(); i ++) {
+			Cliente cliente = clientes.get(i);
+			
+			if(cliente.getCedula().equals(cedula)) {
+				return cliente;
+			}
+		}
+		return null;
+	}
+	
+	
 	
 }
---
+++ src/com/krakedev/artesanal/test/TestClientesMejorado.java
@@ -0,0 +1,17 @@
+package com.krakedev.artesanal.test;
+
+import com.krakedev.artesanal.NegocioMejorado;
+
+public class TestClientesMejorado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 NegocioMejorado negocio =
+	                new NegocioMejorado();
+
+	        negocio.registrarCliente(
+	                "Paul",
+	                "17223344");
+	}
+
+}
